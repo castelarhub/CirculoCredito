37b4a95928bb29eef20f30f990bc8469fbac6752
operador <operador@10.0.2.15>
2014-08-15 16:25:17 -0500

    Ejemplo Basico de un proyecto que crea un provedor web service con Maven.

---
+++ mavenBasicWebService/src/main/java/com/castelar/ws/OperacionAritmetica.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.castelar.ws;
+
+/**
+ *
+ * @author operador
+ */
+public class OperacionAritmetica {
+    
+    public int suma(int x, int y){
+        return x+y;
+    }
+    
+}
