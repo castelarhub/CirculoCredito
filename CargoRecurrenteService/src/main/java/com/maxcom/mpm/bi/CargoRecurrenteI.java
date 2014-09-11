b80790bccd24a326595dc7093218417cbc4f7b67
castelarhub <nelson.castelar.oracle@gmail.com>
2014-09-10 22:57:32 -0500

    Se actualiza proyecto: - Se agregan comentarios al codigo. De momento solo una interfaz.

--- CargoRecurrenteService/src/main/java/com/maxcom/mpm/bi/CargoRecurrenteI.java
+++ CargoRecurrenteService/src/main/java/com/maxcom/mpm/bi/CargoRecurrenteI.java
@@ -1,20 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.maxcom.mpm.bi;
 
 import com.maxcom.mpm.dto.RespuestaTO;
 import com.maxcom.mpm.dto.TransaccionTO;
 
 /**
- *
- * @author Nelson C.
+ * La interfaz CargoRecurrenteI define el punto de entrada para trabajar 
+ * con los cargos recurrentes en el MPM. <br>
+ * 
+ * @author Nelson Castelar
+ * @version 1.0
  */
 public interface CargoRecurrenteI {
     
+    /**
+     * Procesa la transaccion especificada
+     * @param transaccion Transaccion a procesar
+     * @return Regresa la respuesta que resulto del procesamiento. Un objeto de tipo RespuestaTO.
+     * @since 1.0
+     */
     public RespuestaTO procesar(TransaccionTO transaccion);
     
 }
