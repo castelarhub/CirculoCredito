9827d26998d3790cfda53af88cb803f11e729b84
operador <operador@10.0.2.15>
2014-10-16 19:26:51 -0500

    Se actualiza proyecto, se agrega el cliente que consume el web service

--- ResultadoCargoRecurrente/src/main/java/com/maxcom/mpm/controller/NotificacionResultadoFront.java
+++ ResultadoCargoRecurrente/src/main/java/com/maxcom/mpm/controller/NotificacionResultadoFront.java
@@ -1,13 +1,88 @@
 package com.maxcom.mpm.controller;
 
+import com.maxcom.mpm.client.Cargo;
+import com.maxcom.mpm.client.Respuesta;
+import com.maxcom.mpm.client.RespuestaService;
+import com.maxcom.mpm.client.Transaccion;
+import com.maxcom.mpm.dto.CargoTO;
 import com.maxcom.mpm.dto.RespuestaTO;
 import com.maxcom.mpm.dto.TransaccionTO;
 
 public class NotificacionResultadoFront{
 
     public RespuestaTO notificar(TransaccionTO transaccion) {
-        //Not implemented yet.
-        return new RespuestaTO();
+        
+        RespuestaService cargoRecurrente = new RespuestaService();
+        RespuestaTO resp = new RespuestaTO();
+        
+        if(transaccion == null ){
+            
+            resp.setAcuseSap("-");
+            resp.setIdCobranza(0);
+            resp.setIdEstatus("ERROR");
+            resp.setObservaciones("Se requiere una transaccion valida.");
+
+            return resp;            
+            
+        }
+        
+        Transaccion transaccionRC = new Transaccion();
+        transaccionRC.setIdCobranza(transaccion.getIdCobranza());
+        
+        Cargo cargoAux = null;
+        
+        if(transaccion.getListaCargosAceptados() != null){        
+            for(CargoTO cargoX:transaccion.getListaCargosAceptados()){
+
+                cargoAux = new Cargo();
+                cargoAux.setCuenta(cargoX.getCuenta());
+                cargoAux.setDescripcionRespuesta(cargoX.getDescripcionRespuesta());
+                cargoAux.setEmail(cargoX.getEmail());
+                cargoAux.setEntidadFinanciera(true);
+                cargoAux.setIdCobranzaDetalle(cargoX.getIdCobranzaDetalle());
+                cargoAux.setIdRespuestaCargo(cargoX.getIdRespuestaCargo());
+                cargoAux.setImporte(cargoX.getImporte());
+                cargoAux.setMarcaTarjeta(cargoX.getMarcaTarjeta());
+                cargoAux.setNombreCliente(cargoX.getNombreCliente());
+                cargoAux.setReferencia(cargoX.getReferencia());
+                cargoAux.setTipoCuenta(cargoX.getTipoCuenta());
+
+                transaccionRC.getListaCargosAceptados().add(cargoAux);
+            }
+        }
+        
+        if(transaccion.getListaCargosRechazados() != null){
+            for(CargoTO cargoX:transaccion.getListaCargosRechazados()){
+
+                cargoAux = new Cargo();
+                cargoAux.setCuenta(cargoX.getCuenta());
+                cargoAux.setDescripcionRespuesta(cargoX.getDescripcionRespuesta());
+                cargoAux.setEmail(cargoX.getEmail());
+                cargoAux.setEntidadFinanciera(true);
+                cargoAux.setIdCobranzaDetalle(cargoX.getIdCobranzaDetalle());
+                cargoAux.setIdRespuestaCargo(cargoX.getIdRespuestaCargo());
+                cargoAux.setImporte(cargoX.getImporte());
+                cargoAux.setMarcaTarjeta(cargoX.getMarcaTarjeta());
+                cargoAux.setNombreCliente(cargoX.getNombreCliente());
+                cargoAux.setReferencia(cargoX.getReferencia());
+                cargoAux.setTipoCuenta(cargoX.getTipoCuenta());
+
+                transaccionRC.getListaCargosRechazados().add(cargoAux);
+            }
+        }
+        
+        
+        
+        Respuesta respuesta = 
+                cargoRecurrente.getRespuestaCargoRecurrentePort().procesarCargos(transaccionRC);
+                                        
+        
+        resp.setAcuseSap(respuesta.getAcuseSap());
+        resp.setIdCobranza(respuesta.getIdCobranza());
+        resp.setIdEstatus(respuesta.getIdEstatus());
+        resp.setObservaciones(respuesta.getObservaciones());
+        
+        return resp;
     }
     
 }
--- ResultadoCargoRecurrente/src/main/java/com/maxcom/mpm/demo/Demo.java
+++ ResultadoCargoRecurrente/src/main/java/com/maxcom/mpm/demo/Demo.java
@@ -1,15 +1,62 @@
 package com.maxcom.mpm.demo;
 
+
 import com.maxcom.mpm.controller.NotificacionResultadoFront;
+import com.maxcom.mpm.dto.CargoTO;
 import com.maxcom.mpm.dto.RespuestaTO;
 import com.maxcom.mpm.dto.TransaccionTO;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Demo {
     
     public static void main(String ...args){
         NotificacionResultadoFront notificacion = new NotificacionResultadoFront();
-        RespuestaTO respuesta = notificacion.notificar(new TransaccionTO());
-        System.out.print("OK.");
+        
+        List<CargoTO> listaCargosAceptados = new ArrayList<CargoTO>();
+        List<CargoTO> listaCargosRechazados = new ArrayList<CargoTO>();
+        
+        CargoTO cargo = new CargoTO();
+        cargo.setCuenta("123456789012345");
+        cargo.setDescripcionRespuesta("Cargo procesado");
+        cargo.setEmail("nelson.castelar@gmail.com");
+        cargo.setEntidadFinanciera(true);
+        cargo.setIdCobranzaDetalle(1);
+        cargo.setIdRespuestaCargo("00");
+        cargo.setImporte(25.90);
+        cargo.setMarcaTarjeta(2);
+        cargo.setNombreCliente("Nelson Castelar Montiel");
+        cargo.setReferencia("REF-PRUEBA-002");
+        cargo.setTipoCuenta(2);
+        
+        
+        listaCargosAceptados.add(cargo);
+        
+        cargo = new CargoTO();
+        cargo.setCuenta("923456789012345");
+        cargo.setDescripcionRespuesta("Cargo procesado, LA TARJETA NO EXISTE");
+        cargo.setEmail("nelson.castelar@gmail.com");
+        cargo.setEntidadFinanciera(true);
+        cargo.setIdCobranzaDetalle(1);
+        cargo.setIdRespuestaCargo("50");
+        cargo.setImporte(5.50);
+        cargo.setMarcaTarjeta(2);
+        cargo.setNombreCliente("FULANO PEREZ GARCIA");
+        cargo.setReferencia("REF-PRUEBA-003");
+        cargo.setTipoCuenta(2);        
+        
+        listaCargosRechazados.add(cargo);
+        
+        TransaccionTO ejemplo = new TransaccionTO();
+        ejemplo.setIdCobranza(2);
+        ejemplo.setListaCargosAceptados(listaCargosAceptados);
+        ejemplo.setListaCargosRechazados(listaCargosRechazados);
+        
+        
+        RespuestaTO respuesta = notificacion.notificar(ejemplo);
+        
+        System.out.print("respuesta.getObservaciones():"+respuesta.getObservaciones()+"*");
+        
         
     }
     
